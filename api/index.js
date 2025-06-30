e77a6d1384cd218598f04702ff23fb3ea272591e
Mubashir Sohail <mubashirsohail4444@gmail.com>
2025-06-30 17:32:51 +0500

    Set custom views directory for Express app
    Configured the Express app to use '../views' as the views directory, ensuring the correct path is used if the views folder is at the project root. Commented out the unused port variable.

--- api/index.js
+++ api/index.js
@@ -3,8 +3,9 @@ import bodyParser from "body-parser";
 import axios from "axios";
 
 const app = express();
-const port = 3000;
+// const port = 3000;
 app.set("view engine", "ejs");
+app.set("views", "../views"); // adjust path if your views folder is at project root
 
 // Middleware: Path for static files in public folder
 app.use(express.static("public"));
